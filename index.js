048446c243af7b6de2f90e2315d99ce965b9c9a0
Ramon Mangueira <ramonczsilva@gmail.com>
2023-07-24 19:09:43 -0300

    Criando o primeiro comando

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { TOKEN } = process.env;
 
+// importação dos comandos
+const fs = require("node:fs");
+const path = require("node:path");
+
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+console.log(commandsFiles);
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (c) => {
