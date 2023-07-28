ab376b072b22eedb82d9db6a8302a6e47ac90d8e
Ramon Mangueira <ramonczsilva@gmail.com>
2023-07-28 01:36:33 -0300

    fase de teste com selenium

--- Commands/senhaHoje.js
+++ Commands/senhaHoje.js
@@ -1,21 +1,33 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 //configuração do dotenv
 const dotenv = require("dotenv");
 dotenv.config({ path: "../.env" });
-const { EMAIL, SENHA, LINK } = process.env;
+const { EMAIL, SENHA, LINK, LINKPAG } = process.env;
 
-let driver = new Builder().forBrowser("chrome").build();
+//configurando navegador
+const options = new chrome.Options();
+options.addArguments("--headless"); // Executar em modo headless (sem interface gráfica)
+const driver = new Builder()
+  .forBrowser("chrome")
+  .setChromeOptions(options)
+  .build();
 
-driver.get(LINK);
+//Fazer login
+async function run() {
+  await driver.get(LINK);
+  driver.findElement(By.name("email")).sendKeys(EMAIL);
+  driver.findElement(By.name("password")).sendKeys(SENHA, Key.ENTER);
+  driver.wait(until.urlIs(LINKPAG), 5000);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  senhaHoje = await driver
+    .findElement(
+      By.xpath("/html/body/div[1]/div[1]/section[2]/div/div[3]/div/div[2]")
+    )
+    .getText();
+  console.log(senhaHoje);
+  return senhaHoje;
+}
 
-var elemento = driver.findElement(By.name("email"));
-elemento.sendKeys(EMAIL);
-var elemento = driver.findElement(By.name("password"));
-elemento.sendKeys(SENHA, Key.ENTER);
-driver.wait(until.titleIs(SENHA), 100000);
-
-let senhaHoje = driver.findElement(
-  By.xpath("/html/body/div[1]/div[1]/section[2]/div/div[3]/div/div[2]/text()")
-);
-console.log(senhaHoje);
+run();
